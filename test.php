963cb8c31933fac0d74ab48fc0ba540b91603743
Valentin Glinianov <valeglinianov@gmail.com>
2020-04-03 08:23:46 +0300

    minor

--- test.php
+++ test.php
@@ -2,16 +2,9 @@
 <head><title>PHP says hello</title></head>
 <body>
 <b>
-Five plus five is:
 <?php
-print 5 + 5;
+print 'We\'ll  each have a bowl of soup.';
 ?>
-<p>
-Four plus four is:
-<?php
-print 4 + 4;
-?>
-</p>
 </b>
 </body>
 </html>
