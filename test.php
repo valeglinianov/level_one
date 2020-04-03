52fdd8d7c9c39ef5b6b00f5a6f2fdad1386f993b
Valentin Glinianov <valeglinianov@gmail.com>
2020-04-03 08:06:11 +0300

    minor

--- test.php
+++ test.php
@@ -2,10 +2,16 @@
 <head><title>PHP says hello</title></head>
 <body>
 <b>
+Five plus five is:
 <?php
-print "The population of the US is about: ";
-print number_format(320853904);
+print 5 + 5;
 ?>
+<p>
+Four plus four is:
+<?php
+print 4 + 4;
+?>
+</p>
 </b>
 </body>
 </html>
